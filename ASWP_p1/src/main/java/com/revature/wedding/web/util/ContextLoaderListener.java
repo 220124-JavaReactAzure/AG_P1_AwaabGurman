db83df86a291cac2c39ff6c305d726969e3ce70b
Awaab Elamin <AwaabElamin@gmail.com>
2022-02-22 19:12:44 -0600

    update the project to work with Wedding

--- ASWP_p1/src/main/java/com/revature/wedding/web/util/ContextLoaderListener.java
+++ ASWP_p1/src/main/java/com/revature/wedding/web/util/ContextLoaderListener.java
@@ -15,12 +15,15 @@ import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.revature.wedding.dao.EmployeeDAO;
 import com.revature.wedding.dao.ServiceDAO;
 import com.revature.wedding.dao.ServiceTypeDAO;
+import com.revature.wedding.dao.WeddingDAO;
 import com.revature.wedding.services.EmployeeServices;
 import com.revature.wedding.services.ServiceServices;
 import com.revature.wedding.services.ServiceTypesServices;
+import com.revature.wedding.services.WeddingService;
 import com.revature.wedding.servlets.EmployeesServlet;
 import com.revature.wedding.servlets.ServiceServlet;
 import com.revature.wedding.servlets.ServiceTypeServlet;
+import com.revature.wedding.servlets.WeddingServlet;
 @WebListener
 public class ContextLoaderListener implements ServletContextListener {
 
@@ -42,12 +45,16 @@ public class ContextLoaderListener implements ServletContextListener {
 		ServiceServices serviceServices = new ServiceServices(serviceDAO);
 		ServiceServlet serviceServlet = new ServiceServlet(serviceTypesServices, serviceServices, mapper);	
 		
-	
+		WeddingDAO weddingDAO = new WeddingDAO();
+		WeddingService weddingService = new WeddingService(weddingDAO);
+		WeddingServlet weddingServlet = new WeddingServlet(mapper, weddingService);	
 		
 		ServletContext context = sce.getServletContext();
 		context.addServlet("EmployeesServlet", employeeServlet).addMapping("/employees/*");
 		context.addServlet("ServiceTypeServlet", serviceTypeServlet).addMapping("/servicetype/*");
 		context.addServlet("ServiceServlet", serviceServlet).addMapping("/services/*");
+		context.addServlet("WeddingServlet", weddingServlet).addMapping("/wedding/*");
+		
 		
 		
 		
